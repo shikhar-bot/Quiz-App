28e84a2e08782d32179c101773f169d7ead6a549
Shikhar gupta <84851377+shikhar-bot@users.noreply.github.com>
2022-07-23 17:13:51 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,59 @@
+const questions = [
+  {
+    number: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Hyper Tool Markup Language",
+    ],
+  },
+  {
+    number: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Computer Style Sheets",
+      "Creative Style Sheets",
+      "Colorful Style Sheets",
+      "Cascading Style Sheets",
+    ],
+  },
+  {
+    number: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Preprocessed Hypertext Page",
+      "Hypertext Preprocessor",
+      "Processor Hypertext Processor",
+      "None of the above",
+    ],
+  },
+  {
+    number: 4,
+    question:
+      "Which of the below CSS class is used to change the text color of CSS ?",
+    answer: "color",
+    options: [
+      "background-color",
+      "color-background",
+      "color",
+      "None of the above",
+    ],
+  },
+  {
+    number: 5,
+    question:
+      "Which of the below is the correct syntax to put a line over text in CSS ?",
+    answer: "text-decoration: overline",
+    options: [
+      "text-decoration: line",
+      "text-decoration: none",
+      "text-decoration: overline",
+      "text-decoration: underline",
+    ],
+  },
+];
